f74d4fcfcecd266d1352a862f792ac3b199732e7
omaracrystal <omaracrystal@gmail.com>
2015-09-03 12:01:07 -0600

    added html layout for challenge/play page and set up outline for css

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
+
+router.get('/play', function(req, res, next) {
+  res.render('play');
+})
 
 module.exports = router;
