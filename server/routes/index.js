a4e27593fb3ed40107b5101a88206edad3e50ab2
johnny lamb <agnusjack@gmail.com>
2015-09-03 13:11:46 -0600

    npm working in routes, also added ignore file for the keys

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var keys=require('../routes/key');
+var bt = require('../../node_modules/bing-translate/lib/bing-translate.js').init({
+     client_id:keys.client_id,
+     client_secret:keys.client_secret
+   });
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+bt.translate('hello.', 'en', 'es', function(err, response) {
+     res.json(response);
+   });
 });
 
 module.exports = router;
