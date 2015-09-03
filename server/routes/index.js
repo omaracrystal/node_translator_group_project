4718066452d028b5825276b5954b1374a746103d
Luis <delapenl@gmail.com>
2015-09-03 13:56:59 -0600

    first and second page are done

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Language Translator' });
+});
+
+router.post('/submit', function(req, res, next){
+  console.log(req.body.name);
+  if (req.body.name === "") {
+    res.send("Please enter a name");
+  } else {
+  res.redirect('/practice');
+  }
+});
+
+router.get('/practice', function(req, res, next) {
+  res.render('practice', { title: 'Language Translator' });
 });
 
 module.exports = router;
