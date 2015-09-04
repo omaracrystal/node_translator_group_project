124dce12ed9d0c5fce2373fae21c08ef4b5b67ff
omaracrystal <omaracrystal@gmail.com>
2015-09-04 01:22:12 -0600

    fixed some layout added modal got and hid all of the answer area box

--- server/models/user.js
+++ server/models/user.js
@@ -1,15 +1,15 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+// var mongoose = require('mongoose');
+// var Schema = mongoose.Schema;
 
-var User = new Schema ({
-  name: String,
-  challengesTaken: Number,
-  challengesPasses: Number,
-  challengesFailed: Number,
-  numWordsTranslated: Number,
-  numWordsTranslatedCorrectly: Number,
-  numWordsTranslatedIncorrectly: Number
-});
+// var User = new Schema ({
+//   name: String,
+//   challengesTaken: Number,
+//   challengesPasses: Number,
+//   challengesFailed: Number,
+//   numWordsTranslated: Number,
+//   numWordsTranslatedCorrectly: Number,
+//   numWordsTranslatedIncorrectly: Number
+// });
 
-module.exports = mongoose.model('users', User);
-mongoose.connect('mongodb://localhost/translator-users');
+// module.exports = mongoose.model('users', User);
+// mongoose.connect('mongodb://localhost/translator-users');
